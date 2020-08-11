202cad134f0053e6e463810b76bdf99c0954d25c
Enrique Cabrera Flores <enrique.cabflo@gmail.com>
2020-08-11 12:42:45 -0500

    Ruta OpenShift
    Ruta OpenShift

--- WebSphere/AppServer/properties/fsdb/_was_profile_default/default.sh
+++ WebSphere/AppServer/properties/fsdb/_was_profile_default/default.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-WAS_USER_SCRIPT=/jennex/WebSphere/AppServer/profiles/AppSrv01/bin/setupCmdLine.sh
+WAS_USER_SCRIPT=/var/lib/jenkins/jobs/PRUEBA-WS/workspace/WebSphere/AppServer/profiles/AppSrv01/bin/setupCmdLine.sh
 export WAS_USER_SCRIPT
